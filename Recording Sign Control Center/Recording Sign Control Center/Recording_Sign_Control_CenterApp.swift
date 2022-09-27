fc4ed965dda42982143a54e16403f668841f0c42
Jacob Drew <71591438+JakesMD@users.noreply.github.com>
2022-09-27 22:45:52 +0200

    Initial commit

---
+++ Recording Sign Control Center/Recording Sign Control Center/Recording_Sign_Control_CenterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recording_Sign_Control_CenterApp.swift
+//  Recording Sign Control Center
+//
+//  Created by Jacob Drew on 27.09.22.
+//
+
+import SwiftUI
+
+@main
+struct Recording_Sign_Control_CenterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
